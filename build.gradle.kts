284b1841e95d7b85be5ff8cdddd0a0245828a051
cnotgate <49557224+cnotgate@users.noreply.github.com>
2024-05-10 19:42:34 +0700

    Deployment fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.springframework.cloud:spring-cloud-starter-config:4.1.1")
 }
 
 //tasks.test {
